6159ff9fef7d9bae8f30bd39c77302a3aa976248
Obramenko Alexandr <obram22@gmail.com>
2018-05-31 18:32:55 +0300

    kvartal markup

--- js/main.js
+++ js/main.js
@@ -14,6 +14,8 @@ $(function() {
     carousel();
 
     inputMasks();
+
+    animateAnchor();
 })
 
 $(window).resize(function() {
@@ -127,4 +129,43 @@ function carousel() {
 
 function inputMasks() {
     $('.js-phone').mask('+38-000-000-00-00');
+}
+
+
+function animateAnchor() {
+    // Select all links with hashes
+    $('a[href*="#"]')
+
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function(event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function() {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        // $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                            // $target.focus(); // Set focus again
+                        };
+                    });
+                }
+            }
+        });
 }
